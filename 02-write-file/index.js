fede9f19fecb30b5fc3a90964811d297cd732f33
evgeniiDJS <hronologia2019@gmail.com>
2021-11-04 13:40:01 +0400

    feat: add task one & two

---
+++ 02-write-file/index.js
@@ -0,0 +1,8 @@
+const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+stdout.write('Здравствуйте введите текст : ');
+stdin.on('data',  chunk => output.write(chunk));
+process.on('SIGINT', () => {stdout.write('Файл записан, удачи !'); process.exit(); });
